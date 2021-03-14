e1265c3f1564a7e8c09e2ca045ab8cc6904a9b7a
Tei <tei@radioactive.sg>
2021-03-14 14:59:07 +0700

    - Add Firebase config

--- SocialLite/AppDelegate.swift
+++ SocialLite/AppDelegate.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Config Firebase
+        FirebaseApp.configure()
+        // Enable offline
+        Database.database().isPersistenceEnabled = true
+        
         return true
     }
 
